d19067aedbf3c9bb750225868d14d8d1d87964aa
cognettadavide97-afk <cognetta.davide97@gmail.com>
2026-02-08 19:56:35 +0100

    Delete 03-02-2026 directory

--- 03-02-2026/Mattina/Esercizi 2.sql
+++
@@ -1,9 +0,0 @@
-select first_name as NOME from actor;
-select last_name as COGNOME from actor;
-
-select * from film
-where rating in ("G");
-
-select * from customer
-where fist_name like 'A%' and last_name like '%S';
-
